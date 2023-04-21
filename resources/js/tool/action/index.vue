9eaaef1c20d26301d4ad03f7332a8ff836490cfe
Tobias Gepp <tobias.gepp@yesdevs.com>
2023-04-21 12:35:53 +0200

    include more tailwind

--- resources/js/tool/action/index.vue
+++ resources/js/tool/action/index.vue
@@ -7,14 +7,18 @@
       @input="changeBulk"
     >{{ __('Bulk Select') }}</checkbox-with-label>
 
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded bg-primary cursor-pointer shadow-md max-w-full mr-4"
+    <button class="
+flex-shrink-0 shadow rounded focus:outline-none ring-primary-200 dark:ring-gray-600 focus:ring bg-primary-500 hover:bg-primary-400
+active:bg-primary-600 text-white dark:text-gray-800 inline-flex items-center font-bold px-4 h-9 text-sm flex-shrink-0"
             type="button"
             @click="bulkAll"
             v-if="$parent.bulk.enable"
     >{{ __('Select All') }}</button>
 
     <template v-if="$parent.bulkLen()">
-      <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded bg-danger cursor-pointer shadow-md max-w-full mr-4"
+      <button class="
+flex-shrink-0 shadow rounded focus:outline-none ring-red-200 dark:ring-gray-600 focus:ring bg-red-500 hover:bg-red-400
+active:bg-red-600 text-white dark:text-gray-800 inline-flex items-center font-bold px-4 h-9 text-sm flex-shrink-0"
               type="button"
               @click="$parent.deleteFiles(Object.keys($parent.bulk.ids))">
         {{ __('Delete') }} ({{ $parent.bulkLen() }})
